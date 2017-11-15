80d4b03d6654205edab6b5217bca3e64b6cdbfda
Edward Beech <initialed85@gmail.com>
2017-11-15 13:42:52 +0800

    Making GoRuntimeError exception available at top of Python module

--- gosnmp_python/__init__.py
+++ gosnmp_python/__init__.py
@@ -1,5 +1,7 @@
+from common import GoRuntimeError
 from rpc_session import create_snmpv1_session, create_snmpv2c_session, create_snmpv3_session
 
+_ = GoRuntimeError
 _ = create_snmpv1_session
 _ = create_snmpv2c_session
 _ = create_snmpv3_session
