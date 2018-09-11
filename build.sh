e27895495bea770e55aa3f6f5fdee6973e9d4d5d
Edward Beech <initialed85@gmail.com>
2018-09-11 19:28:49 +0800

    Adding support for Go1.11 to build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 which go 2>/dev/null 1>/dev/null
 if [[ $? -ne 0 ]]; then
-    echo "error: failed to find go binary- do you have Go 1.9 or Go 1.10 installed?"
+    echo "error: failed to find go binary- do you have Go 1.9, Go 1.10 or Go1.11 installed?"
     exit 1
 fi
 
 GOVERSION=`go version`
-if [[ $GOVERSION != *"go1.9"* ]] && [[ $GOVERSION != *"go1.10"* ]]; then
+if [[ $GOVERSION != *"go1.9"* ]] && [[ $GOVERSION != *"go1.10"* ]] && [[ $GOVERSION != *"go1.11"* ]]; then
     echo "error: Go version is not 1.9 or 1.10 (was $GOVERSION)"
     exit 1
 fi
